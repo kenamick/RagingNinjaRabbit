055b9916deed34dc9d81fbc7e2baaf06c65b9849
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-06-14 20:11:15 +0200

    Added collision detection

--- src/game.js
+++ src/game.js
@@ -8,14 +8,20 @@ define([], function() {
 		$.getJSON("data/room1.json", function(json) {
 
 			Crafty.e("2D, DOM, TiledMapBuilder").setMapDataSource(json)
-			.createWorld( function( map ){
-			    console.log("done");
-			    });	
+			.createWorld( function( map ) {
+
+					//Obstacles
+					for (var obstacle = 0; obstacle < map.getEntitiesInLayer('Tiles 2').length; obstacle++){
+						map.getEntitiesInLayer('Tiles 2')[obstacle]
+							.addComponent("Collision")
+							.collision();							
+					}
+				});
 
 			$('#loading').hide();
 		});		
 
-        // show FPS
+		// show FPS
         Crafty.e("2D, DOM, FPS").attr({maxValues:10})
         .bind("MessureFPS", function(fps) {
             $('#fps').text('FPS: ' + fps.value);
@@ -24,26 +30,31 @@ define([], function() {
 		// character animation 
 		Crafty.c('CharAnims', {
 			CharAnims: function() {
-		        // setup animations sequences
-		        this.requires("SpriteAnimation, Grid")
-		        .animate("walk_left", [ [0, 96], [32, 96], [64, 96] ])
-		        .animate("walk_right", [ [0, 144], [32, 144], [64, 144] ])
-		        .animate("walk_up", [ [0, 48], [32, 48], [64, 48] ])
-		        .animate("walk_down", [ [0, 0], [32, 0], [64, 0] ]);
-		        return this;
-		    }
+				// setup animations sequences
+				this.requires("SpriteAnimation, Grid")
+				.animate("walk_left", [ [0, 96], [32, 96], [64, 96] ])
+				.animate("walk_right", [ [0, 144], [32, 144], [64, 144] ])
+				.animate("walk_up", [ [0, 48], [32, 48], [64, 48] ])
+				.animate("walk_down", [ [0, 0], [32, 0], [64, 0] ]);
+			return this;
+			}
 		});
 
 		// create character
-		Crafty.e("2D, DOM, player, CharAnims, Multiway, MouseFace")
+		Crafty.e("2D, DOM, Collision, Grid, player, CharAnims, Multiway, MouseFace")
 		.attr({
-		    move: {left: false, right: false, up: false, down: false},
-		    x: 400, y: 256, z:  1,
-		    moving: false
+			move: {left: false, right: false, up: false, down: false},
+			x: 400, y: 256, z:  1,
+			moving: false
 		})
 		.CharAnims()
 		.bind("Moved", function(from) {
 			this.moving = true;
+
+			if( this.hit('obstacles') ) {
+				console.log('collide');
+				this.attr({x: from.x, y:from.y});
+			}
 		})
 		.bind("EnterFrame", function() {
 			// If moving, adjust the proper animation and facing
@@ -61,19 +72,25 @@ define([], function() {
 					break;
 				case this._directions.down:
 					anim = 'walk_down';
-					break;    			
+					break;
 				}
 
-		        if (anim) {
-		        	if (!this.isPlaying(anim))
-		        		this.stop().animate(anim, 8, -1); 
-		    	}
+				if (anim) {
+					if (!this.isPlaying(anim))
+						this.stop().animate(anim, 8, -1); 
+				}
 
-		        this.moving = false;
+				this.moving = false;
 			} else {
 				this.stop();
 			} 
-		}).multiway(2, {W: -90, S: 90, D: 0, A: 180});
+		})
+		.multiway(2, {W: -90, S: 90, D: 0, A: 180})
+		.collision(
+            [8, 40], 
+            [24, 40], 
+            [24, 48], 
+            [8, 48]);
 	});
 
 });
