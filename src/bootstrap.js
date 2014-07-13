d9c0badfb6f242cb4c1b85e4b2fe8bbf233f18a3
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-07-13 15:26:31 +0200

    Add css background

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -24,8 +24,6 @@ require(["src/config.js", "src/game.js"], function(config) {
     Crafty.scene("loading", function() {
         Crafty.load([
             "art/stuz_rabbit.png",
-            "art/stuz_enemy.png",
-
             "art/tiles1.png",
             "art/tiles2.png",
             "art/tiles3.png"
@@ -35,9 +33,9 @@ require(["src/config.js", "src/game.js"], function(config) {
             Crafty.sprite("art/stuz_rabbit.png", {
                 player: [0, 0, 32, 48],
             });
-            Crafty.sprite("art/stuz_enemy.png", {
-                enemy: [0, 0, 32, 48],
-            });
+            // Crafty.sprite("art/stuz_enemy.png", {
+            //     enemy: [0, 0, 32, 48],
+            // });
 
             // Static initializations
 
